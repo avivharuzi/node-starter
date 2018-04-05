666714a08f6c91e70f5d25567d1e9587ea3211e0
avivharuzi <avivharuzi@gmail.com>
2018-04-05 16:24:05 +0300

    Added password helper

---
+++ helpers/password.helper.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+class PasswordHelper {
+    static generateHashPassword(password) {
+        return new Promise((resolve, reject) => {
+            bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+                if (err) {
+                    reject(err);
+                }
+        
+                bcrypt.hash(password, salt, (err, hashPassword) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(hashPassword);
+                });
+            });
+        });
+    }
+
+    static comparePassword(password, hashPassword) {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, hashPassword, (err, isMatch) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(isMatch);
+                }
+            });
+        });
+    }
+}
+
+module.exports = PasswordHelper;
