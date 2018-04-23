703755b15ccdf267ceccf2ce8f4465cf069e325e
avivharuzi <avivharuzi@gmail.com>
2018-04-23 15:56:01 +0300

    server.js changed

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const app = require('./app');
 const http = require('http');
 
+const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
 });
