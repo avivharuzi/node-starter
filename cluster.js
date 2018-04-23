ffba764fd16a33aa91efd23deee69a940702baa6
avivharuzi <avivharuzi@gmail.com>
2018-04-23 16:05:47 +0300

    added cluster.js feature

---
+++ cluster.js
@@ -0,0 +1,14 @@
+const cluster = require('cluster');
+const cpus = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    for (let i = 0; i < cpus; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', () => {
+        cluster.fork();
+    });
+} else {
+    require('./server');
+}
