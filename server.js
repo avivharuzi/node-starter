ff0483ab889b617442c8592d83a657b7311f2c34
avivharuzi <avivharuzi@gmail.com>
2019-03-21 12:41:48 +0200

    Clean files

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 require('dotenv').config();
 
 const app = require('./app');
-const http = require('http');
 
 const hostname = process.env.SERVER_HOSTNAME || '127.0.0.1';
 const port = process.env.SERVER_PORT || 3000;
 
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
